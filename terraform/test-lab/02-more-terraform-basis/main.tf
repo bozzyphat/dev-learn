ec89e76dad8e1c48bbe88408e76a8b7f9109776e
bozzyphat <92072531+bozzyphat@users.noreply.github.com>
2023-06-20 11:12:23 +1200

    update var.tf

--- terraform/test-lab/02-more-terraform-basis/main.tf
+++ terraform/test-lab/02-more-terraform-basis/main.tf
@@ -7,10 +7,6 @@ terraform {
   }
 }
 
-  variable "create_iam_user" {
-    type = string
-    #default = "my_iam_user"
-  }
 
 # Configure the AWS Provider
 provider "aws" {
@@ -21,3 +17,4 @@ resource "aws_iam_user" "my_iam_user" {
   count = 2
   name  = "${var.create_iam_user}_${count.index}"
 }
+
---
+++ terraform/test-lab/02-more-terraform-basis/vars.tf
@@ -0,0 +1,4 @@
+  variable "create_iam_user" {
+    type = string
+    default = "my_iam_user"
+  }
